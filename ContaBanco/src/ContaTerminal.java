fc3c9b73429d54938e39ef9fbaa0afb8f2581bb5
RanieeelB <118456093+RanieeelB@users.noreply.github.com>
2023-09-03 15:29:33 -0300

    Meu projeto conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        String nome = "Raniel";
+        System.out.println("Olá mundo!");
+        System.out.println("Me chamo "+nome+".");
+    }
+}
